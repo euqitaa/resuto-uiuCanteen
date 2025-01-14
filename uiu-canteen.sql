c0b94ceedcf9f81aa67182f8d3cd03ee4b477bf0
Hasnat emad <emad200105@gmail.com>
2025-01-14 21:23:03 +0600

    db

--- uiu-canteen.sql
+++ uiu-canteen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 11, 2025 at 04:58 PM
+-- Generation Time: Jan 14, 2025 at 04:22 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
